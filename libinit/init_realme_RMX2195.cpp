681100f2ee472e81c3e2109e0c261894cd38147f
mnrdnn <udyneos@gmail.com>
2026-03-28 23:44:07 +0700

    fix redefinition

--- libinit/init_realme_RMX2195.cpp
+++ libinit/init_realme_RMX2195.cpp
@@ -9,49 +9,35 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t bengal_global_info = {
+static const variant_info_t RMX2195_global_info = {
     .hwc_value = "",
     .sku_value = "",
 
-    .brand = "Realme",
+    .brand = "realme",
     .device = "bengal",
     .marketname = "Realme C15",
     .model = "RMX2196",
-    .build_fingerprint = "Realme/bengal_global/bengal:11/RKQ1.211001.001/C.16.RGCMIXM:user/release-keys",
+    .build_fingerprint = "realme/bengal_global/bengal:11/RKQ1.211001.001/C.16:user/release-keys",
 
     .nfc = false,
 };
 
-static const variant_info_t bengal_in_info = {
+static const variant_info_t RMX2101_global_info = {
     .hwc_value = "",
     .sku_value = "",
 
-    .brand = "Realme",
+    .brand = "realme",
     .device = "bengal",
-    .marketname = "Realme C15",
-    .model = "RMX2195",
-    .build_fingerprint = "Realme/bengal_global/bengal:11/RKQ1.211001.001/C.16.5.0.RGCMIXM:user/release-keys",
-
-    .nfc = false,
-};
-
-static const variant_info_t bengal_global_info = {
-    .hwc_value = "",
-    .sku_value = "",
-
-    .brand = "Realme",
-    .device = "bengal",
-    .marketname = "Realme C15",
-    .model = "RMX2197",
-    .build_fingerprint = "Realme/bengal_global/bengal:11/RKQ1.211001.001/C.16.7.0.RGKMIXM:user/release-keys",
+    .marketname = "Realme C17",
+    .model = "RMX2101",
+    .build_fingerprint = "realme/bengal_global/bengal:11/RKQ1.211001.001/C.16:user/release-keys",
 
     .nfc = false,
 };
 
 static const std::vector<variant_info_t> variants = {
-    bengal_global_info,
-    bengal_in_info,
-    bengal_global_info,
+    RMX2195_global_info,
+    RMX2101_global_info,
 };
 
 void vendor_load_properties() {
--- lineage_RMX2195.mk
+++ lineage_RMX2195.mk
@@ -14,29 +14,18 @@ $(call inherit-product, vendor/lineage/config/common.mk)
 # Inherit from RMX2195 device
 $(call inherit-product, device/realme/RMX2195/device.mk)
 
-# lineage Stuff
-lineage_OFFICIAL := true
-lineage_GAPPS_TYPE := gapps
-lineage_VARIANT := RELEASE
-
 PRODUCT_NAME := lineage_RMX2195
 PRODUCT_DEVICE := RMX2195
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme C15 Qualcomm
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme C15 Qualcomm Edition
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-TARGET_USES_AOSP_CHARGER := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_INCLUDE_STOCK_ARCORE := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="RMX2195-user 11 RKQ1.211001.001 V13.0.5.0.RGCMIXM release-keys"
+    PRIVATE_BUILD_DESC="bengal-user 11 RKQ1.211001.001 C.16 release-keys"
 
-BUILD_FINGERPRINT := Realme/RMX2195_global/RMX2195:11/RKQ1.211001.001/V13.0.5.0.RGCMIXM:user/release-keys
+BUILD_FINGERPRINT := realme/bengal_global/bengal:11/RKQ1.211001.001/C.16:user/release-keys
